c213e97d1f0514ab365eb8b6f097476fd1011a5c
jack <3hellspawn3@gmail.com>
2020-10-06 21:20:36 +0300

    Project v02

---
+++ educa/routing.py
@@ -0,0 +1,14 @@
+# as Channels expects we define a single root application that 
+# will be executed for all requests.
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
